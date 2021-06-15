6ca68d44647e681098f6f5534091f32d0439c186
coder2711 <73233141+coder2711@users.noreply.github.com>
2021-06-15 14:23:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//https://teachablemachine.withgoogle.com/models/VQ5_uV4FD/
+//https://teachablemachine.withgoogle.com/models/bn96QA1ez/
 
 Webcam.set({
     width : 350,
@@ -19,9 +19,26 @@ function take_picture(){
 
 console.log("ml5 version : " , ml5.version);
 
-classify = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/VQ5_uV4FD/model.json' , model_loaded);
+classify = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/bn96QA1ez/model.json' , model_loaded);
 
 
 function model_loaded(){
 console.log("model is loaded...........")
+}
+
+function Check(){
+    img = document.getElementById("Captured_image");
+    classify.classify(img , gotResults);
+}
+
+function gotResults(error , results){
+   if(error){
+       console.error("error");
+   }
+   else{
+       console.log(results)
+       document.getElementById("result_person_name").innerHTML = results[0].label;
+       accuracy = reults[0].confidence.toFixed(3)*100;
+       document.getElementById("result_person_accuracy").innerHTML = accuracy+"%";
+   }
 }
