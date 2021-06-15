fde072e5366aa0280cc8b5e2b9b5454c6eb12b1b
coder2711 <73233141+coder2711@users.noreply.github.com>
2021-06-15 14:27:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_picture(){
 
 console.log("ml5 version : " , ml5.version);
 
-classify = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/bn96QA1ez/model.json' , model_loaded);
+classified = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/bn96QA1ez/model.json' , model_loaded);
 
 
 function model_loaded(){
@@ -28,17 +28,17 @@ console.log("model is loaded...........")
 
 function Check(){
     img = document.getElementById("Captured_image");
-    classify.classify(img , gotResults);
+    classified.classify(img , Gotresults);
 }
 
-function gotResults(error , results){
-   if(error){
-       console.error("error");
-   }
-   else{
-       console.log(results)
-       document.getElementById("result_person_name").innerHTML = results[0].label;
-       accuracy = reults[0].confidence.toFixed(3)*100;
-       document.getElementById("result_person_accuracy").innerHTML = accuracy+"%";
-   }
+function Gotresults(error , results){
+    if(error){
+        console.error("error");
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_person_name").innerHTML=results[0].label;
+        accuracy=results[0].confidence.toFixed(3)*100;
+        document.getElementById("result_person_accuracy").innerHTML=accuracy+"%";
+    }
 }
